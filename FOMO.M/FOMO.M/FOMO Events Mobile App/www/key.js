fef9ba5de43c12266648b47240758d85782dfa11
Calvin Magezi <30309553+CalvinMagezi@users.noreply.github.com>
2021-06-25 18:09:50 +0300

    Digital Art and logos and stuff

---
+++ FOMO.M/FOMO.M/FOMO Events Mobile App/www/key.js
@@ -0,0 +1,2 @@
+var key = "AIzaSyDxPHIHN0TzERESlQ3KAk7d_efncyOCFOc";
+
---
+++ FOMO.M/FOMO.M/Website/Fomo3.js
@@ -0,0 +1,41 @@
+/* ==========================*/
+/* Overlay Script*/
+/* ==========================*/
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+/* ==========================*/
+/* Slide Show Script*/
+/* ==========================*/
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
